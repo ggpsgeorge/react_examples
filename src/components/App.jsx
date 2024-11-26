2cfa97167a5431ef566441edec440c2257165203
George G <ggpsgeorge@gmail.com>
2024-11-26 16:11:47 -0300

    scroll trial

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Scroller from './Scroller'
 import UserProfile from './UserProfile'
 
 function App() {
 
   return (
     <>
+      <Scroller></Scroller>
       <UserProfile></UserProfile>
       <UserProfile></UserProfile>
       <UserProfile></UserProfile>
---
+++ src/components/Scroller.jsx
@@ -0,0 +1,19 @@
+function Scroller() {
+    
+    const items = ["Whatever", "Sup", "Yeah"]
+    
+    return(
+        <div className="scroller">
+            {items.map((item) => {
+                return(
+                    <div className="item">
+                        <h1>{item}</h1>
+                    </div>
+                );
+            })}
+        </div>
+    );
+
+}
+
+export default Scroller
