d80bfa4dcae29eebef9cee64e07507a16094e52a
George G <ggpsgeorge@gmail.com>
2024-12-09 13:16:53 -0300

    onChange with select tag and get the option value

--- src/components/InputOnChange/InputOnChange.jsx
+++ src/components/InputOnChange/InputOnChange.jsx
@@ -13,12 +13,24 @@ function InputOnChange() {
         setComment(event.target.value)
     }
 
+    const [product, setProduct] = useState("")
+    const handleProduct = (event) => {
+        console.log(event)
+        setProduct(event.target.value)
+    }
+
     return ( 
         <div>
             <input value={name} onChange={handleInputName} placeholder="Your Name"></input>
             <p>Name: {name} </p>
             <textarea value={comment} onChange={handleComment} placeholder="Your comments here"></textarea>
             <p>Text: {comment}</p>
+            <select name={product} onClick={handleProduct}>
+                <option value="Iphone 13">Iphone 13</option>
+                <option value="Playstation 5">Playstation 5</option>
+                <option value="Fiat Uno">Fiat Uno</option>
+            </select>
+            <p>Product: {product}</p>
         </div>
     );
 }
